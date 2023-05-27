9e2ba79e7e927f29bb43f725d5d90a9c4681f682
Altan <suyanovaltan@gmail.com>
2023-05-27 15:39:49 +0300

    button are now changing colors after answer being made

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -7,17 +7,21 @@ const Question = ({ handleOptionClick, question }) => {
   const { options, flagUrl, correctAnswer } = question;
   const [imgVisible, setImgVisible] = useState(false);
   const [isButtonDisabled, setIsButtonDisabled] = useState(false);
+
   const handleButtonClick = (isCorrect) => {
     handleOptionClick(isCorrect);
     setTimeout(() => setImgVisible(false), 3000);
     setIsButtonDisabled(true);
-    setTimeout(() => setIsButtonDisabled(false), 3000);
+    setTimeout(() => {
+      setIsButtonDisabled(false);
+    }, 3000);
   };
 
   useEffect(() => {
     setImgVisible(true);
     console.log('Update');
   })
+
 
   return (
     <Transition
@@ -30,7 +34,9 @@ const Question = ({ handleOptionClick, question }) => {
         <img rel="preload" className={`flagImage`} src={flagUrl} alt='' />
         <div className='answers'>
           {options.map((option, index) => (
-            <button disabled={isButtonDisabled} key={index} onClick={() => handleButtonClick(option === correctAnswer)} className='button'>
+            <button disabled={isButtonDisabled} style={isButtonDisabled ? (option !== correctAnswer ? { backgroundImage: 'linear-gradient(90deg, #d82b56,#be264c,#920c2d)' } : { backgroundImage: 'linear-gradient(to right, rgb(182, 244, 146), rgb(51, 139, 147))' }) : {}} key={index} onClick={() => {
+              handleButtonClick(option === correctAnswer);
+            }} className='button'>
               {`${index + 1}. ${option}`}
             </button>
           ))}
