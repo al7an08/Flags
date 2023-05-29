e60ee190d04e57fcd8a1dea78187950127fc0d3b
maksonchek <56319593+maksonchek@users.noreply.github.com>
2023-05-29 14:22:16 +0300

    Update HelpPage.jsx fixed HelpBage button

--- src/pages/HelpPage.jsx
+++ src/pages/HelpPage.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-const HelpPage = ({ handleHelp }) => {
+const HelpPage = ({ handleStartPage }) => {
     return (
         <div className='helpPage'>
             <h1 className='text'>Правила игры</h1>
             <p className='text'>На экране будет показан флаг страны, Вам необходимо, выбрав один из вариантов, угадать какой стране принадлежит данный флаг. Удачной игры!</p>
-            <button className='button' onClick={handleHelp} style={{
+            <button className='button' onClick={handleStartPage} style={{
                 width: 'fit-content',
             }}>Вернуться назад</button>
         </div>
