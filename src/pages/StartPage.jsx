d498c13dfab64849a99ff09f4a95a026163cb33f
maksonchek <56319593+maksonchek@users.noreply.github.com>
2023-06-01 19:35:06 +0300

    Update StartPage.jsx Не добро пожаловать

--- src/pages/StartPage.jsx
+++ src/pages/StartPage.jsx
@@ -4,7 +4,6 @@ const StartPage = (props) => {
     const { handleStartGame, handleHelp } = props;
     return (
         <div className='startPage'>
-            <h2 className='text'>Добро пожаловать! Начнем играть?</h2>
             <button className='button' onClick={handleStartGame}>Начать игру</button>
             <button className='button' onClick={handleHelp}>Правила</button>
         </div>
