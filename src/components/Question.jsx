12ae4d4a80dc89f925e2a4add637f3ff6e639bac
Altan <suyanovaltan@gmail.com>
2023-06-08 02:06:48 +0300

    another try to fix focus on button

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,17 +1,25 @@
 import React from 'react';
 import { Transition } from 'react-transition-group'
-import { useState, useEffect } from 'react';
+import { useState, useEffect, findDOMNode, useRef } from 'react';
 
 
 const Question = ({ handleOptionClick, question, answer_received }) => {
   const { options, flagUrl, correctAnswer } = question;
   const [imgVisible, setImgVisible] = useState(false);
   const [isButtonDisabled, setIsButtonDisabled] = useState(false);
+  const [focus, setFocus] = useState(false);
+
+  const focusElementReference = useRef < HTMLInputElement > (null);
 
   const handleButtonClick = (isCorrect) => {
     handleOptionClick(isCorrect);
     setTimeout(() => setImgVisible(false), 3000);
     setIsButtonDisabled(true);
+    setFocus(false);
+    setTimeout(() => {
+      setFocus(true);
+      focusElementReference.current?.focus();
+    }, 3000);
     setTimeout(() => {
       setIsButtonDisabled(false);
     }, 3000);
@@ -21,7 +29,7 @@ const Question = ({ handleOptionClick, question, answer_received }) => {
 
   useEffect(() => {
     setImgVisible(true);
-    console.log('Update');
+    console.log(object);
     if (answer_received) {
       setTimeout(() => setImgVisible(false), 3000);
       setIsButtonDisabled(true);
@@ -30,6 +38,8 @@ const Question = ({ handleOptionClick, question, answer_received }) => {
       }, 3000)
     }
   })
+
+  var object = document.getElementById("Button1");
 
 
   return (
@@ -43,7 +53,7 @@ const Question = ({ handleOptionClick, question, answer_received }) => {
         <img rel="preload" className={`flagImage`} src={flagUrl} alt='' />
         <div className='answers'>
           {options.map((option, index) => (
-            <button autoFocus={index === 0 && !isButtonDisabled ? true : false} disabled={isButtonDisabled} style={isButtonDisabled ? (option !== correctAnswer ? { backgroundImage: 'linear-gradient(90deg, #d82b56,#be264c,#920c2d)' } : { backgroundImage: 'linear-gradient(to right, rgb(182, 244, 146), rgb(51, 139, 147))' }) : {}} key={parseInt(index)} onClick={() => {
+            <button id={"Button" + (index + 1)} ref={index === 0 ? focusElementReference : 0} autoFocus={index === 0 && !isButtonDisabled ? true : false} disabled={isButtonDisabled} style={isButtonDisabled ? (option !== correctAnswer ? { backgroundImage: 'linear-gradient(90deg, #d82b56,#be264c,#920c2d)' } : { backgroundImage: 'linear-gradient(to right, rgb(182, 244, 146), rgb(51, 139, 147))' }) : {}} key={parseInt(index)} onClick={() => {
               handleButtonClick(option === correctAnswer);
             }} className='button'>
               {`${parseInt(index) + 1}. ${option}`}
